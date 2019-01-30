d975c5454437ba514999ebb9aaf3204e5b9e1c23
Alejandro Álvarez Ayllón <a.alvarezayllon@gmail.com>
2019-01-30 16:04:22 +0100

    Add boundary test

---
+++ tests/properties/detection/test_flags.py
@@ -0,0 +1,56 @@
+import numpy as np
+from astropy.table import Table
+
+from util import stuff
+
+
+def test_saturated_keywork(sextractorxx, datafiles):
+    """
+    Check if the saturated flag is set based on the image header when the configuration
+    does not specify.
+    """
+    saturated_fits = datafiles / 'simple' / 'saturated.fits'
+    run = sextractorxx(
+        detection_image=saturated_fits,
+        output_properties='SourceIDs,PixelCentroid,SourceFlags',
+        detection_image_saturation=None
+    )
+    assert run.exit_code == 0
+
+    catalog = Table.read(sextractorxx.get_output_catalog())
+
+    assert len(catalog) == 1
+    assert stuff.SourceFlags(catalog['source_flags']) is stuff.SourceFlags.SATURATED
+
+
+def test_saturated_override(sextractorxx, datafiles):
+    """
+    Check if the saturated flag is *NOT* set when we override the detection image saturation
+    """
+    saturated_fits = datafiles / 'simple' / 'saturated.fits'
+    run = sextractorxx(
+        detection_image=saturated_fits,
+        output_properties='SourceIDs,PixelCentroid,SourceFlags',
+        detection_image_saturation=100000
+    )
+    assert run.exit_code == 0
+
+    catalog = Table.read(sextractorxx.get_output_catalog())
+
+    assert len(catalog) == 1
+    assert stuff.SourceFlags(catalog['source_flags']) is stuff.SourceFlags.NONE
+
+
+def test_boundary(sextractorxx, datafiles):
+    saturated_fits = datafiles / 'simple' / 'boundary.fits'
+    run = sextractorxx(
+        detection_image=saturated_fits,
+        output_properties='SourceIDs,PixelCentroid,SourceFlags',
+        threshold_value=5
+    )
+    assert run.exit_code == 0
+
+    catalog = Table.read(sextractorxx.get_output_catalog())
+
+    assert len(catalog) == 3
+    assert np.sum(catalog['source_flags'])
