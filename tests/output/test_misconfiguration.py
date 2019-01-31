27c41db41f3c987844cfe82cb13d1ec49a2c9baa
Alejandro Álvarez Ayllón <a.alvarezayllon@gmail.com>
2019-01-31 14:00:38 +0100

    Add test for no output properties configured

--- tests/output/test_misconfiguration.py
+++ tests/output/test_misconfiguration.py
@@ -1,3 +1,6 @@
+from astropy.table import Table
+
+
 def test_output_directory_missing(sextractorxx, datafiles):
     """
     The output directory does not exist.
@@ -26,3 +29,18 @@ def test_wrong_output_property(sextractorxx, datafiles):
     )
     assert run.exit_code > 0
 
+
+def test_no_output_properties(sextractorxx, datafiles):
+    """
+    No output properties configured. Still, it is expected to get a catalog
+    with some default properties.
+    """
+    single_source_fits = datafiles / 'simple' / 'saturated.fits'
+    run = sextractorxx(
+        detection_image=single_source_fits,
+        output_properties=None
+    )
+    assert run.exit_code == 0
+
+    table = Table.read(sextractorxx.get_output_catalog())
+    assert len(table) == 1
