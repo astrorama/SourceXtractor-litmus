19cb54a78f5fc671a9c62097862cdf454a137a46
Alejandro Álvarez Ayllón <a.alvarezayllon@gmail.com>
2019-02-07 11:42:55 +0100

    Weight test (no python config)

--- tests/single_frame/test_coadded_no_py.py
+++ tests/single_frame/test_coadded_no_py.py
@@ -32,15 +32,15 @@ def coadded_catalog(sextractorxx, datafiles, module_output_area, signal_to_noise
     return np.sort(catalog[bright_filter], order=('world_centroid_alpha', 'world_centroid_delta'))
 
 
-def test_detection(single_frame_catalog, reference):
+def test_detection(coadded_catalog, coadded_reference):
     """
     Test that the number of results matches the reference, and that they are reasonably close
     """
-    assert len(single_frame_catalog) > 0
-    assert len(single_frame_catalog) == len(reference)
+    assert len(coadded_catalog) > 0
+    assert len(coadded_catalog) == len(coadded_reference)
 
 
-def test_location(single_frame_catalog, reference, stuff_simulation):
+def test_location(coadded_catalog, coadded_reference, stuff_simulation):
     """
     The detections should be at least as close as the reference to the truth.
     Single frame simulations are in pixel coordinates.
@@ -48,10 +48,10 @@ def test_location(single_frame_catalog, reference, stuff_simulation):
     _, _, kdtree = stuff_simulation
 
     det_closest = stuff.get_closest(
-        single_frame_catalog, kdtree
+        coadded_catalog, kdtree
     )
     ref_closest = stuff.get_closest(
-        reference, kdtree, alpha='ALPHA_SKY', delta='DELTA_SKY'
+        coadded_reference, kdtree, alpha='ALPHA_SKY', delta='DELTA_SKY'
     )
 
     assert np.mean(det_closest['dist']) <= np.mean(ref_closest['dist'])
@@ -60,20 +60,17 @@ def test_location(single_frame_catalog, reference, stuff_simulation):
 @pytest.mark.parametrize(
     ['flux_column', 'reference_flux_column'], [
         [['isophotal_flux', 'isophotal_flux_err'], ['FLUX_ISO', 'FLUXERR_ISO']],
-        [['auto_flux', 'auto_flux_err'], ['FLUX_AUTO', 'FLUXERR_AUTO']],
-        [['aperture_flux:0:0', 'aperture_flux_err:0:0'], ['FLUX_APER:0', 'FLUXERR_APER:0']],
-        [['aperture_flux:0:1', 'aperture_flux_err:0:1'], ['FLUX_APER:1', 'FLUXERR_APER:1']],
-        [['aperture_flux:0:2', 'aperture_flux_err:0:2'], ['FLUX_APER:2', 'FLUXERR_APER:2']],
+        [['auto_flux', 'auto_flux_err'], ['FLUX_AUTO', 'FLUXERR_AUTO']]
     ]
 )
-def test_fluxes(single_frame_catalog, reference, flux_column, reference_flux_column, tolerances):
+def test_fluxes(coadded_catalog, coadded_reference, flux_column, reference_flux_column, tolerances):
     """
     Cross-validate flux columns. The measured fluxes and errors should be close to the reference.
     """
-    det_flux = get_column(single_frame_catalog, flux_column[0])
-    ref_flux = get_column(reference, reference_flux_column[0])
-    det_err = get_column(single_frame_catalog, flux_column[1])
-    ref_err = get_column(reference, reference_flux_column[1])
+    det_flux = get_column(coadded_catalog, flux_column[0])
+    ref_flux = get_column(coadded_reference, reference_flux_column[0])
+    det_err = get_column(coadded_catalog, flux_column[1])
+    ref_err = get_column(coadded_reference, reference_flux_column[1])
     assert np.isclose(det_flux, ref_flux, rtol=tolerances['flux']).all()
     assert np.isclose(det_err, ref_err, rtol=tolerances['flux_error']).all()
 
@@ -81,13 +78,10 @@ def test_fluxes(single_frame_catalog, reference, flux_column, reference_flux_col
 @pytest.mark.parametrize(
     ['mag_column', 'reference_mag_column'], [
         [['isophotal_mag', 'isophotal_mag_err'], ['MAG_ISO', 'MAGERR_ISO']],
-        [['auto_mag', 'auto_mag_err'], ['MAG_AUTO', 'MAGERR_AUTO']],
-        [['aperture_mag:0:0', 'aperture_mag_err:0:0'], ['MAG_APER:0', 'MAGERR_APER:0']],
-        [['aperture_mag:0:1', 'aperture_mag_err:0:1'], ['MAG_APER:1', 'MAGERR_APER:1']],
-        [['aperture_mag:0:2', 'aperture_mag_err:0:2'], ['MAG_APER:2', 'MAGERR_APER:2']],
+        [['auto_mag', 'auto_mag_err'], ['MAG_AUTO', 'MAGERR_AUTO']]
     ]
 )
-def test_magnitude(single_frame_catalog, reference, mag_column, reference_mag_column, stuff_simulation, tolerances):
+def test_magnitude(coadded_catalog, coadded_reference, mag_column, reference_mag_column, stuff_simulation, tolerances):
     """
     Cross-validate the magnitude columns. The measured magnitudes should be at least as close
     to the truth as the reference catalog (within a tolerance).
@@ -96,24 +90,24 @@ def test_magnitude(single_frame_catalog, reference, mag_column, reference_mag_co
     expected_mags = np.append(stars.mag, galaxies.mag)
 
     det_closest = stuff.get_closest(
-        single_frame_catalog, kdtree
+        coadded_catalog, kdtree
     )
     ref_closest = stuff.get_closest(
-        reference, kdtree, alpha='ALPHA_SKY', delta='DELTA_SKY'
+        coadded_reference, kdtree, alpha='ALPHA_SKY', delta='DELTA_SKY'
     )
 
-    det_mag = get_column(single_frame_catalog[det_closest['catalog']], mag_column[0])
-    ref_mag = get_column(reference[ref_closest['catalog']], reference_mag_column[0])
+    det_mag = get_column(coadded_catalog[det_closest['catalog']], mag_column[0])
+    ref_mag = get_column(coadded_reference[ref_closest['catalog']], reference_mag_column[0])
     det_mag_diff = expected_mags[det_closest['source']] - det_mag
     ref_mag_diff = expected_mags[ref_closest['source']] - ref_mag
     assert np.mean(np.abs(det_mag_diff)) <= np.mean(np.abs(ref_mag_diff)) * (1 + tolerances['magnitude'])
 
 
-def test_generate_report(single_frame_catalog, reference, stuff_simulation, datafiles, module_output_area):
+def test_generate_report(coadded_catalog, coadded_reference, stuff_simulation, datafiles, module_output_area):
     """
     Not quite a test. Generate a PDF report to allow for better insights.
     """
     plot.generate_report(
-        module_output_area / 'report.pdf', stuff_simulation, datafiles / 'sim09' / 'sim09_r_01.fits',
-        single_frame_catalog, reference
+        module_output_area / 'report.pdf', stuff_simulation, datafiles / 'sim09' / 'sim09_r.fits',
+        coadded_catalog, coadded_reference
     )
