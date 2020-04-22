84c13066ac5983833ebf6dd7bc57b2f2a8297610
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-04-22 08:34:25 +0200

    Add tests for the smoothing box

--- tests/single_frame/test_background.py
+++ tests/single_frame/test_background.py
@@ -7,8 +7,7 @@ import pytest
 )
 def test_background_cellsize(sourcextractor, datafiles, module_output_area, cell_size):
     """
-    Run sourcextractor with different cell sizes: from 0 (which should disable background
-    modelling) to the full image, to more than the image.
+    Run sourcextractor with different cell sizes: up to the full image, and more than the image.
     This is a regression test: cell sizes bigger than the image size caused a segfault
     """
     sourcextractor.set_output_directory(module_output_area)
@@ -40,3 +39,44 @@ def test_background_bad_cellsize(sourcextractor, datafiles, module_output_area, 
     )
     assert run.exit_code == 1
     assert 'There are value(s) < 1 in backgound-cell-size: {}'.format(cell_size) in run.stderr
+
+
+@pytest.mark.regression
+@pytest.mark.parametrize(
+    ['smoothing_box'], [[0], [1], [3], [7]]
+)
+def test_background_smoothing_box(sourcextractor, datafiles, module_output_area, smoothing_box):
+    """
+    Run sourcextractor with different smoothing boxesizes: from 1 (no smoothing) to more than cells are.
+    Note: 1536 / 256 = 6
+    """
+    sourcextractor.set_output_directory(module_output_area)
+
+    run = sourcextractor(
+        output_properties='SourceIDs,PixelCentroid,WorldCentroid',
+        detection_image=datafiles / 'sim11' / 'img' / 'sim11_r_01.fits.gz',
+        background_cell_size=256,
+        smoothing_box_size=smoothing_box,
+        log_file='sourcextractor_{}.log'.format(smoothing_box)
+    )
+    assert run.exit_code == 0
+
+
+@pytest.mark.regression
+@pytest.mark.parametrize(
+    ['smoothing_box'], [[-1]]
+)
+def test_background_bad_smoothing_box(sourcextractor, datafiles, module_output_area, smoothing_box):
+    """
+    If the smoothing box is < 0, then sourcextractor is expected to fail with a descriptive error
+    """
+    sourcextractor.set_output_directory(module_output_area)
+
+    run = sourcextractor(
+        output_properties='SourceIDs,PixelCentroid,WorldCentroid',
+        detection_image=datafiles / 'sim11' / 'img' / 'sim11_r_01.fits.gz',
+        smoothing_box_size=smoothing_box,
+        log_file='sourcextractor_{}.log'.format(smoothing_box)
+    )
+    assert run.exit_code == 1
+    assert 'There are value(s) < 1 in smoothing-box-size: {}'.format(smoothing_box) in run.stderr
